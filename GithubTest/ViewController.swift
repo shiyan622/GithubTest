219aca14259d402c1148b6790c931f99fe468429
shiyan <1035162183@qq.com>
2020-08-21 16:55:18 -0400

    add to GitHub

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
    @IBAction func buttonClicked(_ sender: UIButton) {
-      print("You finished!")
+      print("You finished! ")
     }
 
 }
