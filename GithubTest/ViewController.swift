6b242efa86c46a20b39c6a51995d23ca00675da2
shiyan <1035162183@qq.com>
2020-08-21 05:48:30 -0400

    change button message

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func buttonClicked(_ sender: UIButton) {
-      print("This is a Git tutorial")
+   @IBAction func buttonClicked(_ sender: UIButton) {
+      print("You finished!")
     }
 
 }
